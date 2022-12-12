2acf6eb967555ba455b7f034e351fa5680841255
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-12-12 23:40:21 +0300

    day3 and day4

---
+++ src/day3.rs
@@ -0,0 +1,95 @@
+use std::collections::HashSet;
+
+use super::Solution;
+
+pub struct Day3;
+
+impl Day3 {
+    const fn renormalize_char(ch: char) -> i32 {
+        let code = ch as i32;
+        match ch {
+            'a'..='z' => code - 'a' as i32 + 1,
+            'A'..='Z' => code - 'A' as i32 + 27,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Solution for Day3 {
+    fn part1(&self, input: Vec<String>) -> i32 {
+        let mut sum = 0;
+
+        for line in input {
+            debug_assert_eq!(line.len() % 2, 0);
+            let (part1, part2) = line.split_at(line.len() / 2);
+            let part1 = part1.chars().collect::<HashSet<char>>();
+            let part2 = part2.chars().collect::<HashSet<char>>();
+            let intersection = part1.intersection(&part2);
+
+            sum += intersection
+                .map(|&ch| Self::renormalize_char(ch))
+                .sum::<i32>()
+        }
+
+        sum
+    }
+
+    fn part2(&self, input: Vec<String>) -> i32 {
+        let mut sum = 0;
+
+        for line in input.chunks(3) {
+            let (part1, part2, part3) = match line {
+                [line1, line2, line3] => (line1, line2, line3),
+                _ => unreachable!(),
+            };
+            let part1 = part1.chars().collect::<HashSet<char>>();
+            let part2 = part2.chars().collect::<HashSet<char>>();
+            let part3 = part3.chars().collect::<HashSet<char>>();
+            let intersection: HashSet<_> = part1.intersection(&part2).copied().collect();
+            let intersection = intersection.intersection(&part3);
+
+            sum += intersection
+                .map(|&ch| Self::renormalize_char(ch))
+                .sum::<i32>()
+        }
+
+        sum
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pre_part1_test() {
+        let solution = Day3;
+        let demo_input = crate::read_lines("day3_demo.txt");
+
+        assert_eq!(solution.part1(demo_input), 157);
+    }
+
+    #[test]
+    fn part1_test() {
+        let solution = Day3;
+        let demo_input = crate::read_lines("day3_part1.txt");
+
+        assert_eq!(solution.part1(demo_input), 7763);
+    }
+
+    #[test]
+    fn pre_part2_test() {
+        let solution = Day3;
+        let demo_input = crate::read_lines("day3_demo.txt");
+
+        assert_eq!(solution.part2(demo_input), 70);
+    }
+
+    #[test]
+    fn part2_test() {
+        let solution = Day3;
+        let demo_input = crate::read_lines("day3_part1.txt");
+
+        assert_eq!(solution.part2(demo_input), 2569);
+    }
+}
---
+++ src/day4.rs
@@ -0,0 +1,95 @@
+use std::str::FromStr;
+
+use super::Solution;
+
+pub struct Day4;
+
+struct Range {
+    from: i32,
+    to: i32,
+}
+
+impl FromStr for Range {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (from, to) = s.split_once('-').unwrap();
+
+        Ok(Range {
+            from: from.parse().unwrap(),
+            to: to.parse().unwrap(),
+        })
+    }
+}
+impl Range {
+    fn from_line(line: String) -> (Range, Range) {
+        let (part1, part2) = line.split_once(',').unwrap();
+
+        (part1.parse().unwrap(), part2.parse().unwrap())
+    }
+    fn fully_contain(&self, other: &Self) -> bool {
+        self.from >= other.from && self.to <= other.to
+    }
+    fn overlaps(&self, other: &Self) -> bool {
+        self.from <= other.to && other.from <= self.to
+    }
+}
+
+impl Solution for Day4 {
+    fn part1(&self, input: Vec<String>) -> i32 {
+        input
+            .into_iter()
+            .map(Range::from_line)
+            .filter(|(part1, part2)| part1.fully_contain(part2) || part2.fully_contain(part1))
+            .count()
+            .try_into()
+            .unwrap()
+    }
+
+    fn part2(&self, input: Vec<String>) -> i32 {
+        input
+            .into_iter()
+            .map(Range::from_line)
+            .filter(|(part1, part2)| part1.overlaps(part2))
+            .count()
+            .try_into()
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pre_part1_test() {
+        let solution = Day4;
+        let demo_input = crate::read_lines("day4_demo.txt");
+
+        assert_eq!(solution.part1(demo_input), 2);
+    }
+
+    #[test]
+    fn part1_test() {
+        let solution = Day4;
+        let demo_input = crate::read_lines("day4_part1.txt");
+
+        assert_eq!(solution.part1(demo_input), 542);
+    }
+
+    #[test]
+    fn pre_part2_test() {
+        let solution = Day4;
+        let demo_input = crate::read_lines("day4_demo.txt");
+
+        assert_eq!(solution.part2(demo_input), 4);
+    }
+
+    #[test]
+    fn part2_test() {
+        let solution = Day4;
+        let demo_input = crate::read_lines("day4_part1.txt");
+
+        assert_eq!(solution.part2(demo_input), 900);
+    }
+}
